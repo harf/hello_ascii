7f241f2deeee5aa7e8f4070b9d463278cedc3730
haduki <base_loop@yahoo.co.jp>
2013-05-21 13:56:22 +0900

    ループとasc[]のサイズ修正

--- hello.c
+++ hello.c
@@ -3,22 +3,23 @@ int main(void)
 {	
 	char topx = 0x40;
 	char undx = 0x00;
-	char *fifo,asc[6] = {0x0f, 0x0c, 0x0c, 0x05, 0x08};
-	char *lifo,obj[6];
-	char x,i;
+	char *fifo,asc[5] = {0x0f, 0x0c, 0x0c, 0x05, 0x08};
+	char *lifo,obj[6];	//NULLが最初にきてしまうよ。なんてこったい\(^^)/
+	char x,i,j;
 	
 	lifo = &obj[0];
 	fifo = &asc[0];
 
-	while(*fifo != '\0'){
+	for(j = 0; j < sizeof(asc); j++){
 		for(i = 1; i < 27; i++){
 			x = (undx + i) | topx;
 			if(x == (topx | *fifo)){
 				*lifo = x;	
 				lifo++;
-				fifo++;
+				break;
 			}
 		}
+	fifo++;
 	}
 	for(i = 0; i < sizeof(obj); i++){
 		printf("%c", *lifo);
