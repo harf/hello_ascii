431adf3d4627335d683ed5a0c3f7b5a3cd963497
haduki <base_loop@yahoo.co.jp>
2013-05-21 16:30:58 +0900

    値修正

--- hello.c
+++ hello.c
@@ -4,11 +4,11 @@ int main(void)
 	char topx = 0x40;
 	char undx = 0x00;
 	char *fifo,asc[5] = {0x0f, 0x0c, 0x0c, 0x05, 0x08};
-	char *lifo,obj[6];	//NULLが最初にきてしまうよ。なんてこったい\(^^)/
+	char *lifo,obj[5];
 	char x,i,j;
 	
 	fifo = &asc[0];
-	lifo = &obj[1];
+	lifo = &obj[0];
 
 	for(j = 0; j < sizeof(asc); j++){
 		for(i = 1; i < 27; i++){
@@ -21,7 +21,7 @@ int main(void)
 		}
 	fifo++;
 	}
-	for(i = 1; i < sizeof(obj); i++){
+	for(i = 0; i < sizeof(obj); i++){
 		lifo--;
 		printf("%c", *lifo);
 	}
